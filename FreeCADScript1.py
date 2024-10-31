2867e938f6c300addfc1d15c932ab68fbf5239f1
Pradeep Tamma <Pradeep.tamma@outlook.com>
2024-10-30 23:33:44 -0700

    FreeCAD Script working

--- FreeCADScript1.py
+++ FreeCADScript1.py
@@ -1,8 +1,8 @@
 import yaml
 import numpy as np
-# import FreeCAD as App
-# import Part
-# import DraftGeomUtils
+import FreeCAD as App
+import Part
+import DraftGeomUtils
 
 #defs
 crn_x=[]
@@ -21,24 +21,24 @@ crn_z = np.zeros(len(crn_x))
 # crn_y = data['ydata']
 vtx = np.stack((crn_x,crn_y,crn_z)).T
 
-print((vtx[0]),(vtx[1]))
-print((vtx[2]),(vtx[3]))
+# print((vtx[0]),(vtx[1]))
+# print((vtx[1]),(vtx[1]))
 
-# apnt1 = App.Vector()
-# apnt2 = App.Vector(vtx[1])
-# edge1 = Part.makeLine(apnt1,apnt2)
-# apnt3 = App.Vector(vtx[2])
-# apnt4 = App.Vector(vtx[3])
-# edge2 = Part.makeLine(apnt3,apnt4)
-# edge2 = Part.makeLine((10, 0, 0), (10, 10, 0))
-# wire1 = Part.Wire([edge1, edge2]) 
-# S1 = Part.Shape([edge1,edge2])
+apnt1 = App.Vector(vtx[0])
+apnt2 = App.Vector(vtx[1])
+edge1 = Part.makeLine(apnt1,apnt2)
+apnt3 = App.Vector(vtx[2])
+apnt4 = App.Vector(vtx[3])
+edge2 = Part.makeLine(apnt2,apnt3)
+wire1 = Part.Wire([edge1, edge2]) 
+S1 = Part.Shape([edge1,edge2])
+
 # W = Part.Wire(S1.Edges)
 # edge3 = Part.makeLine((10, 10, 0), (-2, 10, 0))
 # edge4 = Part.makeLine((-2, 10, 0), (-2, 0, 0))
 # wire2 = Part.Wire([edge3, edge4])
 # wire3 = Part.Wire([wire1, wire2])
-# 
-# S2= DraftGeomUtils.filletWire(wire3, 0.5, chamfer=True)
-# 
-# Part.show(edge2)
+
+S2= DraftGeomUtils.filletWire(wire1, 0.5, chamfer=True)
+
+Part.show(S2)
