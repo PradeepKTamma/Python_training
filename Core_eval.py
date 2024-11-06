c59a7076d42d610fa9d6a44721c9083220af7e11
Pradeep Tamma <Pradeep.tamma@outlook.com>
2024-11-06 15:48:21 -0800

    updated

--- Core_eval.py
+++ Core_eval.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 N = []
 L = []
+Lup = []
+Llo = []
 
 n = len(sys.argv)
 if n == 2:
@@ -30,9 +32,18 @@ Al = data['Core']['AL']
 for i in range(10):
     N.insert(i,2*i) 
     L.insert(i,N[i]**2 * Al)
+    Lup.insert(i,L[i]*1.2)
+    Llo.insert(i,L[i]*0.9)
 
 print(L,N)
 
 fig, ax = plt.subplots()
-ax.plot(N,L, color='red', linestyle='-', linewidth=2)
+ax.plot(N,L, color='red', linestyle='-', linewidth=2, label="Typ")
+ax.plot(N,Llo, color='red',linestyle='--',linewidth=2, label="LL")
+ax.plot(N,Lup, color='red',linestyle='-.',linewidth=2, label="UL")
+ax.set_xlim([0, 2*len(L)])
+ax.set_ylim([0,600])
+ax.set_title("Inductor vs No. of Turns")
+plt.legend()
+plt.grid()
 plt.show()
